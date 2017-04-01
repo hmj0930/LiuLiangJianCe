ebfd44c8c41d6695e2c446854d0581c9bc42e6fc
HanMJ <915660592@qq.com>
2017-04-01 09:49:28 +0800

    Initial Commit

---
+++ LiuLiangJianCe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiuLiangJianCe
+//
+//  Created by MJ on 2017/4/1.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiuLiangJianCe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiuLiangJianCe
+//
+//  Created by MJ on 2017/4/1.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
